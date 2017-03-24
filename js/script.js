98983a7e85ca7158978a6c561a168736acf2765d
sveiki@viktoraslava.lt <sveiki@viktoraslava.lt>
2017-03-25 01:30:10 +0200

    adding animations

--- js/script.js
+++ js/script.js
@@ -3,14 +3,22 @@
 /*************************************
 	Variables init
 **************************************/
-var state;
+
 var clock = {
-	'time': {
-		minutes:0,
-		seconds:0,
-		miliseconds:0
-	}
-}
+		'time': {
+			minutes:0,
+			seconds:0,
+			miliseconds:0
+		},
+		'lapCounter': 0,
+		'lap': {
+			minutes:0,
+			seconds:0,
+			miliseconds:0
+		}
+	},
+	laps = new Array(),
+	state;
 
 //loaderiui imesti https://projects.lukehaas.me/css-loaders/
 //http://codepen.io/giana/pen/yYBpVY/
@@ -29,7 +37,7 @@ function reset(){
 	clock.time.minutes = 0;
 	clock.time.seconds = 0;
 	clock.time.miliseconds = 0;
-	
+
 	pause.id = 'start';
 	window.clearInterval(state);
 	pause.querySelector('i').classList.toggle('fa-play');
@@ -39,7 +47,95 @@ function reset(){
 	timer.querySelector('.miliseconds').innerHTML = '00';
 	showHide(false); 
 
+}
 
+function addLine(){
+	var list = document.querySelector('#lap-list ul'),
+		newList = document.createElement('li');
+
+		newList.innerHTML = "<p class='slide-in'></p>";
+		newList.querySelector('p').innerHTML = '<span class="lap-number">#3</span><span class="first-lap">0 01.99</span><span class="second-lap">0 08.36</span>';
+
+		list.prepend(newList);
+}
+
+function startLap(){
+
+	//alert('starting lap');
+	//this mean we started the lap!
+	//if(lapCounter != 0){
+
+	//}
+
+		addLine();
+
+	var lap = new Object(),
+		start = new Object(),
+		end = new Object(),
+		chronoState;
+
+		//clock.lap.minutes = 0;
+		//clock.lap.seconds = 0;
+		//clock.lap.miliseconds = 0;
+
+		window.clearInterval(chronoState);
+		chronoState = window.setInterval(doLapChrono, 10); //išsiaškinti, kodėl kas 10
+
+		end.minutes = clock.time.minutes;
+		end.seconds = clock.time.seconds;
+		end.miliseconds = clock.time.miliseconds;
+
+	lap.start = clock.lap;
+	lap.end = end;
+
+	laps.push(lap);
+
+	console.log(laps);
+
+	console.log('start:' + lap.start.minutes + ' ' + lap.start.seconds + ' ' + lap.start.miliseconds);
+	console.log('end:' + lap.end.minutes + ' ' + lap.end.seconds + ' ' + lap.end.miliseconds);
+
+	clock.lapCounter++;
+}
+
+function doLapChrono(){
+
+	var time = clock.lap,
+		miliseconds = null;
+
+	time.miliseconds++;
+
+	//jei milisekundės yra daugiau už 999, pridedame vieną sekundę
+	if(time.miliseconds > 99){
+		time.miliseconds = 0;
+		time.seconds++;
+	}
+
+	if(time.seconds > 59){
+		time.seconds = 0;
+		time.minutes++;
+	}
+
+	clock.lap.minutes = time.minutes;
+	clock.lap.seconds = time.seconds;
+	clock.lap.miliseconds = time.miliseconds;
+}
+
+function shareLapSwitch(){
+
+	var button = document.querySelector('.js-lap-share');
+
+	if(button.id == 'lap'){
+		button.id = 'share';
+		button.innerHTML = 'Share';
+		button.removeEventListener('click', startLap);
+	}
+	else{
+		button.id = 'lap';
+		button.innerHTML = 'Lap';
+		button.addEventListener('click', startLap);
+
+	}
 
 }
 
@@ -79,6 +175,8 @@ function start(){
 		//jei chronometras paleistas, jį turime sustabdyti
 	}
 
+	shareLapSwitch();
+
 	this.querySelector('i').classList.toggle('fa-play');
 	this.querySelector('i').classList.toggle('fa-pause');
 
@@ -104,6 +202,11 @@ function doChrono(){
 		time.minutes++;
 	}
 
+	clock.time.miliseconds = time.miliseconds;
+	clock.time.seconds = time.seconds;
+	clock.time.minutes = time.minutes;
+
+	/* styling the miliseconds */
 	if(time.miliseconds < 9){
 		miliseconds = '0' + time.miliseconds;
 	}
@@ -136,5 +239,6 @@ document.addEventListener('DOMContentLoaded', function(){
 
 	document.querySelector('#reset').addEventListener('click', reset);
 	document.querySelector('#start').addEventListener('click', start);
+
 
 });
